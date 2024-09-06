360537f5e82ad8c84f24f6ef7a49e17ba4c5cb8e
apreia <106892008+apreia@users.noreply.github.com>
2024-09-05 22:27:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+      titulo: "Via Láctea",
+      descricao: "A Via Láctea, nossa galáxia natal, é uma imensa estrutura em espiral que abriga bilhões de estrelas, incluindo o nosso Sol. Com um diâmetro estimado em cerca de 100.000 anos-luz.",
+      link: "https://pt.wikipedia.org/wiki/Via_L%C3%A1ctea"
+    },
+    {
+      titulo: "Andrômeda",
+      descricao: "Andrômeda é a galáxia espiral mais próxima da Via Láctea. É visível a olho nu em noites escuras e claras, e é um dos objetos mais distantes que podem ser vistos sem auxílio de instrumentos ópticos.",
+      link: "https://pt.wikipedia.org/wiki/Galáxia_de_Andrômeda"
+    },
+    {
+      titulo: "Sombrero",
+      descricao: "A galáxia do Sombrero é famosa por sua aparência distinta, lembrando um chapéu mexicano. Ela é uma galáxia lenticular, com um disco central brilhante e um halo de estrelas mais antigas.",
+      link: "https://pt.wikipedia.org/wiki/Galáxia_do_Sombrero"
+    },
+    {
+      titulo: "Rodamoinho (M51)",
+      descricao: "A Galáxia do Rodamoinho é uma das galáxias mais famosas e fotogênicas. Sua estrutura espiral é extremamente definida e ela está interagindo com uma galáxia menor.",
+      link: "https://pt.wikipedia.org/wiki/Galáxia_do_Rodamoinho"
+    },
+    {
+      titulo: "Cigarro (M82)",
+      descricao: "A Galáxia de Cigarro é uma galáxia espiral barrada que está passando por um período de intensa formação estelar. Sua aparência alongada e irregular a faz lembrar um charuto.",
+      link: "https://pt.wikipedia.org/wiki/Galáxia_do_Cigarro"
+    }
+  ];
