84d805d393fec7d7e1792e3c2feb7c1e95ec0e36
apreia <106892008+apreia@users.noreply.github.com>
2024-09-08 11:36:07 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,23 +2,45 @@ function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa) {
+        section.innerHTML = "Nada foi encontrado"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
-        // Cria um novo elemento div para cada resultado
-        resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
 
-                <p class="descricao-meta">${dado.descricao}</p>
+        if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemento div para cada resultado
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
 
-                <a href=${dado.link} target="_blank">Mais Informações</a>
-            </div>
-        `;
+                    <p class="descricao-meta">${dado.descricao}</p>
+
+                    <a href=${dado.link} target="_blank">Mais Informações</a>
+                </div>
+            `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "Nada foi encontrado"
     }
 
     // Atribui os resultados gerados ao conteúdo da seção
--- dados.js
+++ dados.js
@@ -1,32 +1,32 @@
 let dados = [
-    {
-      titulo: "Via Láctea",
-      descricao: "A Via Láctea, nossa galáxia natal, é uma imensa estrutura em espiral que abriga bilhões de estrelas, incluindo o nosso Sol. Com um diâmetro estimado em cerca de 100.000 anos-luz.",
-      link: "https://pt.wikipedia.org/wiki/Via_L%C3%A1ctea"
-    },
-    {
-      titulo: "Andrômeda",
-      descricao: "Andrômeda é a galáxia espiral mais próxima da Via Láctea. É visível a olho nu em noites escuras e claras, e é um dos objetos mais distantes que podem ser vistos sem auxílio de instrumentos ópticos.",
-      link: "https://pt.wikipedia.org/wiki/Galáxia_de_Andrômeda"
-    },
-    {
-      titulo: "Sombrero",
-      descricao: "A galáxia do Sombrero é famosa por sua aparência distinta, lembrando um chapéu mexicano. Ela é uma galáxia lenticular, com um disco central brilhante e um halo de estrelas mais antigas.",
-      link: "https://pt.wikipedia.org/wiki/Galáxia_do_Sombrero"
-    },
-    {
-      titulo: "Rodamoinho (M51)",
-      descricao: "A Galáxia do Rodamoinho é uma das galáxias mais famosas e fotogênicas. Sua estrutura espiral é extremamente definida e ela está interagindo com uma galáxia menor.",
-      link: "https://pt.wikipedia.org/wiki/Galáxia_do_Rodamoinho"
-    },
-    {
-      titulo: "Cigarro (M82)",
-      descricao: "A Galáxia de Cigarro é uma galáxia espiral barrada que está passando por um período de intensa formação estelar. Sua aparência alongada e irregular a faz lembrar um charuto.",
-      link: "https://pt.wikipedia.org/wiki/Galáxia_do_Cigarro"
-    },
-    {
-    titulo: "Nebulosa de Orion",
-    descricao: "Uma das regiões de formação de estrelas mais próximas da Terra. É visível a olho nu em condições adequadas.",
-    link: "https://pt.wikipedia.org/wiki/Nebulosa_de_Orion"
+  {
+    titulo: "Via Láctea",
+    descricao: "A Via Láctea, nossa galáxia natal, é uma imensa estrutura em espiral que abriga bilhões de estrelas, incluindo o nosso Sol. Com um diâmetro estimado em cerca de 100.000 anos-luz.",
+    link: "https://pt.wikipedia.org/wiki/Via_L%C3%A1ctea",
+    tags: "espiral natal local"
+  },
+  {
+    titulo: "Andrômeda",
+    descricao: "Andrômeda é a galáxia espiral mais próxima da Via Láctea. É visível a olho nu em noites escuras e claras, e é um dos objetos mais distantes que podem ser vistos sem auxílio de instrumentos ópticos.",
+    link: "https://pt.wikipedia.org/wiki/Galáxia_de_Andrômeda",
+    tags: "espiral vizinha grande"
+  },
+  {
+    titulo: "Sombrero",
+    descricao: "A galáxia do Sombrero é famosa por sua aparência distinta, lembrando um chapéu mexicano. Ela é uma galáxia lenticular, com um disco central brilhante e um halo de estrelas mais antigas.",
+    link: "https://pt.wikipedia.org/wiki/Galáxia_do_Sombrero",
+    tags: "lenticular disco halo"
+  },
+  {
+    titulo: "Rodamoinho (M51)",
+    descricao: "A Galáxia do Rodamoinho é uma das galáxias mais famosas e fotogênicas. Sua estrutura espiral é extremamente definida e ela está interagindo com uma galáxia menor.",
+    link: "https://pt.wikipedia.org/wiki/Galáxia_do_Rodamoinho",
+    tags: "espiral interagindo fotogênica"
+  },
+  {
+    titulo: "Cigarro (M82)",
+    descricao: "A Galáxia de Cigarro é uma galáxia espiral barrada que está passando por um período de intensa formação estelar. Sua aparência alongada e irregular a faz lembrar um charuto.",
+    link: "https://pt.wikipedia.org/wiki/Galáxia_do_Cigarro",
+    tags: "espiral barrada formação estelar irregular"
   }
-  ];
+];
